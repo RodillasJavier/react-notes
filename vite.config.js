b03f150e570da2ff7e95dd2fcb1282ea2c12b2f0
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-15 23:11:58 -0400

    completed initial Linting section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ ],
+  plugins: [
+    eslint(),
+  ],
 });
