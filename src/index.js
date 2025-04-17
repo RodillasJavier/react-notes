4826c08719fc4aa7d4878a50ee362f511998507d
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-16 21:54:42 -0400

    finished the setup section of the assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import $ from 'jquery';
 import './style.scss';
 
 let time = 0;
 setInterval(() => {
-  $('#main').html(`You've been on this page for ${time} seconds.`);
+  document.getElementById('main').innerHTML = `You've been on this page for ${time} seconds.`;
   time += 1;
 }, 1000);
