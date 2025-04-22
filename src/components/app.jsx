a602374e3be5275e347c2a47662aac7c5b8880b0
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-22 10:46:40 -0400

    Update .gitignore and other changes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 function App(props) {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([{
+    title: 'testing',
+    text: 'I is a note',
+    x: '400',
+    y: '12',
+    zIndex: '26',
+  }, {
+    title: 'title',
+    text: 'text',
+    x: '1',
+    y: '1',
+    zIndex: '1',
+  }]);
   return (
     <div>Bruh Moment</div>
   );
