fafa9dde5ca04ef326a2da0883bbca488e9b4c33
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-22 11:53:06 -0400

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,52 @@
 import React, { useState } from 'react';
+import { produce } from 'immer';
 
 function App(props) {
-  const [notes, setNotes] = useState([{
-    title: 'testing',
-    text: 'I is a note',
-    x: '400',
-    y: '12',
-    zIndex: '26',
-  }, {
-    title: 'title',
-    text: 'text',
-    x: '1',
-    y: '1',
-    zIndex: '1',
-  }]);
+  const [notes, setNotes] = useState({
+    // For testing
+    1: {
+      title: 'testing',
+      text: 'I is a note',
+      x: '400',
+      y: '12',
+      zIndex: '26',
+    },
+    2: {
+      title: 'title',
+      text: 'text',
+      x: '1',
+      y: '1',
+      zIndex: '1',
+    },
+  });
+
+  // 'Update Notes' function
+  const updateNotes = (noteID, fields) => {
+    setNotes(
+      produce((draft) => {
+        draft.noteID = { ...draft.notes.noteID, ...fields };
+      }),
+    );
+  };
+
+  // 'Delete Note' function
+  const deleteNote = (noteID) => {
+    setNotes(
+      produce((draft) => {
+        delete draft[noteID];
+      }),
+    );
+  };
+
+  // 'Add Note' function
+  const addNote = (noteID, newNote) => {
+    setNotes(
+      produce((draft) => {
+        draft[noteID] = newNote;
+      }),
+    );
+  };
+
   return (
     <div>Bruh Moment</div>
   );
